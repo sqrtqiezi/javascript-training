9d9e163fd171b1e1ce37b5787fb12b4f6448598b
sqrtqiezi <njin.cool@gmail.com>
2018-01-09 10:30:50 +0800

    init project: douban movie

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Todos from './todos/todos'
+import Login from './login/login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <h1>App</h1>
+          <ul>
+            <li><Link to="/">Todo List</Link></li>
+            <li><Link to="/login">Login Form</Link></li>
+          </ul>
+          <Switch>
+            <Route exact path="/" component={Todos}/>
+            <Route path="/login" component={Login}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ todo-list/src/todos/todos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="todos">
+        <h2>ToDO List</h2>
+    </div>
+  );
+}
