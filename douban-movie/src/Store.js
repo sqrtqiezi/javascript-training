fd75eab0198aaea7b833660259ec282828b47ecf
sqrtqiezi <njin.cool@gmail.com>
2018-01-09 13:29:54 +0800

    chore: set dev tools

--- douban-movie/src/Store.js
+++ douban-movie/src/Store.js
@@ -1,9 +1,21 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 
 import {reducer as menuReducer} from './footer_menu';
+
+const win = window;
 
 const reducer = combineReducers({
   menu: menuReducer
 });
 
-export default createStore(reducer);
+const middlewares = [];
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(require('redux-immutable-state-invariant').default());
+}
+
+const storeEnhancers = compose(
+  applyMiddleware(...middlewares),
+  (win && win.devToolsExtension) ? win.devToolsExtension() : (f) => f,
+);
+
+export default createStore(reducer, {}, storeEnhancers);
